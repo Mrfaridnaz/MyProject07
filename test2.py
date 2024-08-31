df2451c6ecb7510359700ac8d26f7f0cced85736
mrfaridnaz <mrfad8392@gmail.com>
2024-08-31 20:31:39 +0530

    ulter the code

--- test2.py
+++ test2.py
@@ -1,11 +1,5 @@
-# Get input from the user as a comma-separated string of numbers
-numbers = input("Enter a list of numbers separated by commas: ")
+x = 3
+y = 4
 
-# Convert the string into a list of integers
-numbers_list = [int(num.strip()) for num in numbers.split(",")]
-
-# Calculate the sum of the numbers in the list
-total_sum = sum(numbers_list)
-
-# Print the result
-print(f"The sum of the numbers in the list is: {total_sum}")
+z = x + y
+print(z)
